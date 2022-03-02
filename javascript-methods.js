08fc73bf0d0515de9b1efaa36443514b480ab6de
johnnylaicode <jl8338@yahoo.com>
2022-03-01 22:39:38 -0500

    Added javascript-method.js

---
+++ javascript-methods.js
@@ -0,0 +1,49 @@
+// FOR EACH //
+Array.prototype.myEach = function() {
+  // Place your code here.
+};
+
+// MAP //
+Array.prototype.myMap = function() {
+  // Place your code here.
+};
+
+// SOME //
+Array.prototype.mySome = function() {
+  // Place your code here.
+};
+
+// REDUCE //
+Array.prototype.myReduce = function() {
+  // Place your code here.
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function() {
+  // Place your code here.
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function() {
+  // Place your code here.
+};
+
+// PUSH //
+Array.prototype.myPush = function() {
+  // Place your code here.
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function() {
+  // Place your code here.
+};
+
+// KEYS //
+Object.myKeys = function() {
+  // Place your code here.
+};
+
+// VALUES //
+Object.myValues = function() {
+  // Place your code here.
+};
