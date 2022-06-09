752dc2f171b1f7181afdd55394bd76cdd23b62cc
johnnylaicode <jl8338@yahoo.com>
2022-06-09 10:04:31 -0400

    Updated files.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # javascript-methods-starter-template-code
 Starter template code for Assignment 2 - JavaScript Methods. 
+
+## JavaScript Prototype: Method ##
+Every object in JavaScript has a built-in property called "prototype." 
+
+The prototype constructor is used to add new methods (functions) and properties to a JavaScript object. 
+- If a method is constructed, then it will be available for the object. 
+- If a property is constructed, then the object will be given the property and its value, as default.
+
+In this Assignment, we use the prototype constructor to add new methods to the Array() object.
--- javascript-methods.js
+++ javascript-methods.js
@@ -1,3 +1,13 @@
+/*----------------------------------------------------------
+JavaScript Prototype: Method
+Every object in JavaScript has a built-in property called "prototype." 
+The prototype constructor is used to add new methods (functions) and properties to a JavaScript object. 
+- If a method is constructed, then it will be available for the object. 
+- If a property is constructed, then the object will be given the property and its value, as default.
+
+In this Assignment, we use the prototype constructor to add new methods to the Array() object.
+----------------------------------------------------------*/
+
 // MAP //
 Array.prototype.myMap = function(callbackFn) {
   // Place your code here.
