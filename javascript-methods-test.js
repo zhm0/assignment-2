a792225d9c09acce38724dc10bcdbe408c882d4b
Mike Zhang <zhangmike0@gmail.com>
2023-02-27 22:37:03 -0500

    Created and tested myFilter

--- javascript-methods-test.js
+++ javascript-methods-test.js
@@ -26,3 +26,28 @@ console.log(test_array.map( (x, i, myArray) => myArray[i] * x));
 console.log(test_array);
 console.log(test_array.myMap( (x, i, myArray) => myArray[i] * x));
 console.log(test_array);
+
+//myFilter Test
+console.log('\n|myFilter Test|');
+const { myFilter } = require('./javascript-methods.js');
+
+//Test for 1 parameter
+console.log('\nTest 1:');
+console.log(test_array.filter(x => x > 5));
+console.log(test_array);
+console.log(test_array.myFilter(x => x > 5));
+console.log(test_array);
+
+//Test for 2 parameters
+console.log('\nTest 2:');
+console.log(test_array.filter( (x, i) => x + i > 5));
+console.log(test_array);
+console.log(test_array.myFilter( (x, i) => x + i > 5));
+console.log(test_array);
+
+//Test for 3 parameters
+console.log('\nTest 3:');
+console.log(test_array.filter( (x, i, myArray) => myArray[i] * x > 25));
+console.log(test_array); 
+console.log(test_array.myFilter( (x, i, myArray) => myArray[i] * x > 25));
+console.log(test_array);
